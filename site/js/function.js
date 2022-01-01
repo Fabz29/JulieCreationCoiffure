39bb49a65363bff725967d4d759f2aaa842f5461
Fabien <fabien.zanetti@allotools.com>
2022-01-01 16:45:40 +0100

    Finishing most of the work

--- site/js/function.js
+++ site/js/function.js
@@ -96,10 +96,23 @@ $(document).ready(function () {
 
     //date picker function
     $('#datePicker').datepicker({
-        format: 'dd/mm/yyyy',
+        format: 'mm/dd/yyyy',
         autoclose: true,
         clearBtn: true,
-        todayHighlight: true
+        todayHighlight: true,
+        closeText: 'Fermer',
+        prevText: '&#x3c;Préc',
+        nextText: 'Suiv&#x3e;',
+        currentText: 'Aujourd\'hui',
+        monthNames: ['Janvier','Fevrier','Mars','Avril','Mai','Juin',
+            'Juillet','Aout','Septembre','Octobre','Novembre','Decembre'],
+        monthNamesShort: ['Jan','Fev','Mar','Avr','Mai','Jun',
+            'Jul','Aou','Sep','Oct','Nov','Dec'],
+        dayNames: ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
+        dayNamesShort: ['Dim','Lun','Mar','Mer','Jeu','Ven','Sam'],
+        dayNamesMin: ['Di','Lu','Ma','Me','Je','Ve','Sa'],
+        weekHeader: 'Sm',
+        dateFormat: 'dd-mm-yy',
     })
         //.on('changeDate', function(e) {
         // Revalidate the date field

9efc93d8bdb7fe9300246f512e98d9f6e01217f2
Fabien <fabien.zanetti@allotools.com>
2022-01-02 16:06:36 +0100

    Add certbos script

--- certbot.sh
+++ certbot.sh
@@ -1,9 +1,9 @@
 docker run -it --rm --name certbot \
-    --env AWS_ACCESS_KEY_ID=XXXXXXXXXX \
-    --env AWS_SECRET_ACCESS_KEY=XXXXXXXXXXX \
-    -v "/home/ubuntu/PhpstormProjects/JulierCreationCoiffure/certbot/letsencrypt/:/etc/letsencrypt/" \
-    -v "/home/ubuntu/PhpstormProjects/JulierCreationCoiffure/certbot/letsencrypt/:/var/lib/letsencrypt/" \
+    --env AWS_ACCESS_KEY_ID=xxxxxxxxxxxxxxxxxxx \
+    --env AWS_SECRET_ACCESS_KEY=xxxxxxxxxxxxxx \
+    -v "/xxxxxxxxxx/certbot/letsencrypt/:/etc/letsencrypt/" \
+    -v "/xxxxxxxxxx/certbot/letsencrypt/:/var/lib/letsencrypt/" \
     certbot/dns-route53 certonly \
-    -d julie-creation-coiffure.fr \
-    -m XXXXXXXXXXXX \
+    -d xxxxxxxxx \
+    -m xxxxxxxxxxxxx \
     --agree-tos --server https://acme-v02.api.letsencrypt.org/directory
